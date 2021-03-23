7409444aede26826bbf03b5540df943cb424d513
Barış Demirezen <baris@barisdemirezen.com>
2021-03-24 01:10:10 +0300

    make it better

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 let connected = [];
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
 });
 
-io.on('connection', (socket) => {
-  socket.on('userJoined', user => {
-    let newUser = {id: socket.id, name: user};
+io.on("connection", (socket) => {
+  socket.on("userJoined", (user) => {
+    let newUser = { id: socket.id, name: user };
     connected.push(newUser);
-    io.emit('joined', user);
+    io.emit("joined", user);
   });
 });
 
-io.on('connection', (socket) => {
-  socket.on('disconnect', () => {
-    let leavedIndex = connected.findIndex( e => e.id == socket.id);
-    if(leavedIndex){
-      io.emit('leaved', connected[leavedIndex].name);
-      connected.splice(leavedIndex,1); 
+io.on("connection", (socket) => {
+  socket.on("disconnect", () => {
+    let leavedIndex = connected.findIndex((e) => e.id == socket.id);
+    if (leavedIndex != null) {
+      io.emit("leaved", connected[leavedIndex].name);
+      connected.splice(leavedIndex, 1);
+      io.emit("userList", connected);
     }
-     
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("updateUsers", () => {
+    io.emit("userList", connected);
   });
 });
 
